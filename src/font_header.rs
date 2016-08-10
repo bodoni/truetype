0e7f2cd064b5a29fb24b63b9483832958069dcee
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-10 19:29:18 +0200

    Rename Flags to FontFlags

--- src/font_header.rs
+++ src/font_header.rs
@@ -4,35 +4,35 @@
 
 use q32;
 
+flags! {
+    #[doc = "Font-header flags."]
+    pub FontFlags(u16) {
+        0b0000_0000_0000_0001 => is_baseline_at_0,
+        0b0000_0000_0000_0010 => is_left_side_bearing_at_0,
+        0b1000_0000_0000_0000 => is_invalid,
+    }
+}
+
 table! {
     #[doc = "A font header."]
     #[derive(Copy)]
     pub FontHeader {
-        version              (q32  ) = { q32(0x00010000) }, // version
-        revision             (q32  ), // fontRevision
-        checksum_adjustment  (u32  ), // checkSumAdjustment
-        magic_number         (u32  ) = { 0x5F0F3CF5 }, // MagicNumber
-        flags                (Flags), // flags
-        units_per_em         (u16  ), // unitsPerEm
-        created              (i64  ), // created
-        modified             (i64  ), // modified
-        min_x                (i16  ), // xMin
-        min_y                (i16  ), // yMin
-        max_x                (i16  ), // xMax
-        max_y                (i16  ), // yMax
-        mac_style            (u16  ), // macStyle
-        lowest_ppem          (u16  ), // lowestRecPPEM
-        direction_hint       (i16  ), // fontDirectionHint
-        glyph_mapping_format (i16  ), // indexToLocFormat
-        glyph_data_format    (i16  ), // glyphDataFormat
-    }
-}
-
-flags! {
-    #[doc = "Font-header flags."]
-    pub Flags(u16) {
-        0b0000_0000_0000_0001 => is_baseline_at_0,
-        0b0000_0000_0000_0010 => is_left_side_bearing_at_0,
-        0b1000_0000_0000_0000 => is_invalid,
+        version              (q32      ) = { q32(0x00010000) }, // version
+        revision             (q32      ), // fontRevision
+        checksum_adjustment  (u32      ), // checkSumAdjustment
+        magic_number         (u32      ) = { 0x5F0F3CF5 }, // MagicNumber
+        flags                (FontFlags), // flags
+        units_per_em         (u16      ), // unitsPerEm
+        created              (i64      ), // created
+        modified             (i64      ), // modified
+        min_x                (i16      ), // xMin
+        min_y                (i16      ), // yMin
+        max_x                (i16      ), // xMax
+        max_y                (i16      ), // yMax
+        mac_style            (u16      ), // macStyle
+        lowest_ppem          (u16      ), // lowestRecPPEM
+        direction_hint       (i16      ), // fontDirectionHint
+        glyph_mapping_format (i16      ), // indexToLocFormat
+        glyph_data_format    (i16      ), // glyphDataFormat
     }
 }
