782c4648a1dbd9c98714c7af13b73b4d621d03e0
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-10 10:26:57 +0200

    Make a cosmetic adjustment

--- src/macros.rs
+++ src/macros.rs
@@ -43,11 +43,11 @@ macro_rules! flags {
         impl $crate::Value for $structure {
             #[inline(always)]
             fn read<T: $crate::Tape>(tape: &mut T) -> $crate::Result<Self> {
-                let flags = $structure(try!(tape.take::<$kind>()));
-                if flags.is_invalid() {
+                let value = $structure(try!(tape.take::<$kind>()));
+                if value.is_invalid() {
                     raise!("found malformed flags");
                 }
-                Ok(flags)
+                Ok(value)
             }
         }
 
