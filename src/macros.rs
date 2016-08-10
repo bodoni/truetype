1e6a6161a1a3ccea55f378388b9f6b3d756f1d8b
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-10 13:01:41 +0200

    Make a cosmetic adjustment

--- src/macros.rs
+++ src/macros.rs
@@ -71,41 +71,39 @@ macro_rules! table {
         table! { @define $(#[$attribute])* pub $name { $($field ($($kind)+),)* } }
         table! {
             @implement
-            pub $name { $($field ($($kind)+) $(= $value)* $(|$($argument),+| $body)*,)* }
+            pub $name { $($field ($($kind)+) [$($value)*] $(|$($argument),+| $body)*,)* }
         }
     );
-    (@define $(#[$attribute:meta])* pub $name:ident {
-        $($field:ident ($kind:ty),)*
-    }) => (
+    (@define $(#[$attribute:meta])* pub $name:ident { $($field:ident ($kind:ty),)* }) => (
         $(#[$attribute])*
         #[derive(Clone, Debug, Eq, PartialEq)]
         pub struct $name { $(pub $field: $kind,)* }
     );
     (@implement pub $name:ident {
-        $($field:ident ($($kind:tt)+) $(= $value:block)* $(|$($argument:tt),+| $body:block)*,)*
+        $($field:ident ($($kind:tt)+) [$($value:block)*] $(|$($argument:tt),+| $body:block)*,)*
     }) => (
         impl $crate::Value for $name {
             fn read<T: $crate::Tape>(tape: &mut T) -> $crate::Result<Self> {
                 let mut table: $name = unsafe { ::std::mem::uninitialized() };
                 $({
-                    let value = table!(@read $name, table, tape, [$($kind)+]
-                                       $(= $value)* $(|$($argument),+| $body)*);
+                    let value = table!(@read $name, table, tape [$($kind)+] [$($value)*]
+                                       $(|$($argument),+| $body)*);
                     ::std::mem::forget(::std::mem::replace(&mut table.$field, value));
                 })*
                 Ok(table)
             }
         }
     );
-    (@read $name:ident, $this:ident, $tape:ident, [$kind:ty]) => (try!($tape.take()));
-    (@read $name:ident, $this:ident, $tape:ident, [$kind:ty] = $value:block) => ({
+    (@read $name:ident, $this:ident, $tape:ident [$kind:ty] []) => (try!($tape.take()));
+    (@read $name:ident, $this:ident, $tape:ident [$kind:ty] [$value:block]) => ({
         let value = try!($tape.take());
         if value != $value {
             raise!("found a malformed or unsupported table");
         }
         value
     });
-    (@read $name:ident, $this:ident, $tape:ident, [$kind:ty] |$this_:pat, $tape_:pat|
-                                                             $body:block) => ({
+    (@read $name:ident, $this:ident, $tape:ident [$kind:ty] []
+     |$this_:pat, $tape_:pat| $body:block) => ({
         #[inline(always)]
         fn read<T: $crate::Tape>($this_: &$name, $tape_: &mut T) -> $crate::Result<$kind> $body
         try!(read(&$this, $tape))
