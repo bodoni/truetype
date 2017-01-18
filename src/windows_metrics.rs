c144a8475ceefdf459ffb0291cdf7861a96c5dc9
Vladimir Vukicevic <vladimir@pobox.com>
2017-01-18 10:17:09 -0500

    Add support for v0-5 of the OS/2 table

--- src/windows_metrics.rs
+++ src/windows_metrics.rs
@@ -4,18 +4,96 @@
 
 use {Result, Tape, Value};
 
-/// OS/2 and Windows metrics.
+/// OS/2 and Windows metrics.  The version identifies the layout
+/// of the table, with the same layout being shared by multiple
+/// versions in some cases.
 #[derive(Clone, Debug)]
 pub enum WindowsMetrics {
-    /// Version 3.0.
-    Version3(WindowsMetrics3),
-    /// Version 5.0.
+    Version0(WindowsMetrics0),
+    Version1(WindowsMetrics1),
+    Version2(WindowsMetrics2),
+    Version3(WindowsMetrics2),
+    Version4(WindowsMetrics2),
     Version5(WindowsMetrics5),
 }
 
 table! {
-    #[doc = "OS/2 and Windows metrics of version 3.0."]
-    pub WindowsMetrics3 {
+    #[doc = "OS/2 and Windows metrics of version 0."]
+    pub WindowsMetrics0 {
+        version               (u16           ), // version
+        average_char_width    (i16           ), // xAvgCharWidth
+        weight_class          (u16           ), // usWeightClass
+        width_class           (u16           ), // usWidthClass
+        type_flags            (TypeFlags     ), // fsType
+        subscript_x_size      (i16           ), // ySubscriptXSize
+        subscript_y_size      (i16           ), // ySubscriptYSize
+        subscript_x_offset    (i16           ), // ySubscriptXOffset
+        subscript_y_offset    (i16           ), // ySubscriptYOffset
+        superscript_x_size    (i16           ), // ySuperscriptXSize
+        superscript_y_size    (i16           ), // ySuperscriptYSize
+        superscript_x_offset  (i16           ), // ySuperscriptXOffset
+        superscript_y_offset  (i16           ), // ySuperscriptYOffset
+        strikeout_size        (i16           ), // yStrikeoutSize
+        strikeout_position    (i16           ), // yStrikeoutPosition
+        family_class          (i16           ), // sFamilyClass
+        panose                ([u8; 10]      ), // panose
+        unicode_range1        (u32           ), // ulUnicodeRange1
+        unicode_range2        (u32           ), // ulUnicodeRange2
+        unicode_range3        (u32           ), // ulUnicodeRange3
+        unicode_range4        (u32           ), // ulUnicodeRange4
+        vendor_id             ([i8; 4]       ), // achVendID
+        selection_flags       (SelectionFlags), // fsSelection
+        first_char_index      (u16           ), // usFirstCharIndex
+        last_char_index       (u16           ), // usLastCharIndex
+        typographic_ascender  (i16           ), // sTypoAscender
+        typographic_descender (i16           ), // sTypoDescender
+        typographic_line_gap  (i16           ), // sTypoLineGap
+        windows_ascender      (u16           ), // usWinAscent
+        windows_descender     (u16           ), // usWinDescent
+    }
+}
+
+table! {
+    #[doc = "OS/2 and Windows metrics of version 1."]
+    pub WindowsMetrics1 {
+        version               (u16           ), // version
+        average_char_width    (i16           ), // xAvgCharWidth
+        weight_class          (u16           ), // usWeightClass
+        width_class           (u16           ), // usWidthClass
+        type_flags            (TypeFlags     ), // fsType
+        subscript_x_size      (i16           ), // ySubscriptXSize
+        subscript_y_size      (i16           ), // ySubscriptYSize
+        subscript_x_offset    (i16           ), // ySubscriptXOffset
+        subscript_y_offset    (i16           ), // ySubscriptYOffset
+        superscript_x_size    (i16           ), // ySuperscriptXSize
+        superscript_y_size    (i16           ), // ySuperscriptYSize
+        superscript_x_offset  (i16           ), // ySuperscriptXOffset
+        superscript_y_offset  (i16           ), // ySuperscriptYOffset
+        strikeout_size        (i16           ), // yStrikeoutSize
+        strikeout_position    (i16           ), // yStrikeoutPosition
+        family_class          (i16           ), // sFamilyClass
+        panose                ([u8; 10]      ), // panose
+        unicode_range1        (u32           ), // ulUnicodeRange1
+        unicode_range2        (u32           ), // ulUnicodeRange2
+        unicode_range3        (u32           ), // ulUnicodeRange3
+        unicode_range4        (u32           ), // ulUnicodeRange4
+        vendor_id             ([i8; 4]       ), // achVendID
+        selection_flags       (SelectionFlags), // fsSelection
+        first_char_index      (u16           ), // usFirstCharIndex
+        last_char_index       (u16           ), // usLastCharIndex
+        typographic_ascender  (i16           ), // sTypoAscender
+        typographic_descender (i16           ), // sTypoDescender
+        typographic_line_gap  (i16           ), // sTypoLineGap
+        windows_ascender      (u16           ), // usWinAscent
+        windows_descender     (u16           ), // usWinDescent
+        code_page_range1      (u32           ), // ulCodePageRange1
+        code_page_range2      (u32           ), // ulCodePageRange2
+    }
+}
+
+table! {
+    #[doc = "OS/2 and Windows metrics of version 2/3/4."]
+    pub WindowsMetrics2 {
         version               (u16           ), // version
         average_char_width    (i16           ), // xAvgCharWidth
         weight_class          (u16           ), // usWeightClass
@@ -57,7 +135,7 @@ table! {
 }
 
 table! {
-    #[doc = "OS/2 and Windows metrics of version 5.0."]
+    #[doc = "OS/2 and Windows metrics of version 5."]
     pub WindowsMetrics5 {
         version                  (u16           ), // version
         average_char_width       (i16           ), // xAvgCharWidth
@@ -118,9 +196,13 @@ flags! {
 impl Value for WindowsMetrics {
     fn read<T: Tape>(tape: &mut T) -> Result<Self> {
         Ok(match try!(tape.peek::<u16>()) {
+            0 => WindowsMetrics::Version0(try!(tape.take())),
+            1 => WindowsMetrics::Version1(try!(tape.take())),
+            2 => WindowsMetrics::Version2(try!(tape.take())),
             3 => WindowsMetrics::Version3(try!(tape.take())),
+            4 => WindowsMetrics::Version4(try!(tape.take())),
             5 => WindowsMetrics::Version5(try!(tape.take())),
-            _ => raise!("found an unknown format of the OS/2 and Windows metrics"),
+            v => raise!(format!("found an unknown version ({}) of the OS/2 and Windows metrics", v)),
         })
     }
 }
