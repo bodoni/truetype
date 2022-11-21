a5a3aacc9f4f8ae349cf4345be114223193bc50e
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-11-21 18:48:53 +0100

    Update two tests

--- tests/offset_table.rs
+++ tests/offset_table.rs
@@ -17,9 +17,11 @@ mod kaushan_script {
         let OffsetTable { header, records } = ok!(OffsetTable::read(&mut file));
         assert_eq!(header.table_count, 18);
         assert_eq!(records.len(), 18);
-        for record in records.iter() {
-            assert_eq!(record.checksum, ok!(record.checksum(&mut file)));
-        }
+        let (_, failures): (Vec<_>, Vec<_>) = records
+            .iter()
+            .map(|record| (record.tag, record.checksum, ok!(record.checksum(&mut file))))
+            .partition(|(_, left, right)| left == right);
+        assert_eq!(failures.len(), 0, "{:?}", failures);
     }
 }
 
@@ -37,10 +39,15 @@ mod source_serif {
         assert_eq!(header.table_count, 12);
         assert_eq!(header.search_range, 8 * 16);
         assert_eq!(header.entry_selector, 3);
-        assert_eq!(header.range_shift, header.table_count * 16 - header.search_range);
+        assert_eq!(
+            header.range_shift,
+            header.table_count * 16 - header.search_range
+        );
         assert_eq!(records.len(), 12);
-        for record in records.iter() {
-            assert_eq!(record.checksum, ok!(record.checksum(&mut file)));
-        }
+        let (_, failures): (Vec<_>, Vec<_>) = records
+            .iter()
+            .map(|record| (record.tag, record.checksum, ok!(record.checksum(&mut file))))
+            .partition(|(_, left, right)| left == right);
+        assert_eq!(failures.len(), 0, "{:?}", failures);
     }
 }
