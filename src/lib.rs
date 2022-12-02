d054b9dae012a409cbc10c9275ef92809bf8a917
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-02 17:37:08 +0100

    Move basics to typeface

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 //! Parser for TrueType fonts.
 
 #[macro_use]
-mod macros;
+extern crate typeface;
 
 mod number;
 mod tag;
-mod tape;
 
 pub mod char_mapping;
 pub mod font_header;
@@ -19,6 +18,8 @@ pub mod offset_table;
 pub mod postscript;
 pub mod windows_metrics;
 
+pub use typeface::{Error, Result, Tape, Value, Walue};
+
 pub use char_mapping::CharMapping;
 pub use font_header::FontHeader;
 pub use glyph_data::GlyphData;
@@ -31,33 +32,7 @@ pub use number::{q16, q32};
 pub use offset_table::OffsetTable;
 pub use postscript::PostScript;
 pub use tag::Tag;
-pub use tape::{Tape, Value, Walue};
 pub use windows_metrics::WindowsMetrics;
-
-/// An error.
-pub type Error = std::io::Error;
-
-/// An error caused by another error.
-#[derive(Debug)]
-pub struct ErrorWithSource {
-    pub description: String,
-    pub source: Error,
-}
 
 /// A glyph identifier.
 pub type GlyphID = u16;
-
-/// A result.
-pub type Result<T> = std::io::Result<T>;
-
-impl std::fmt::Display for ErrorWithSource {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(formatter, "{}, due to {}", self.description, self.source)
-    }
-}
-
-impl std::error::Error for ErrorWithSource {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(&self.source)
-    }
-}
