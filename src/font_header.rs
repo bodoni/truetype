b53a7f050dfa0dbeb3d0b93e6e58a09d784bc694
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-19 10:22:30 +0100

    Add Macintosh flags to the font header

--- src/font_header.rs
+++ src/font_header.rs
@@ -8,24 +8,24 @@ table! {
     #[doc = "A font header."]
     #[derive(Copy)]
     pub FontHeader {
-        major_version        (u16  ) = { 1 }, // majorVersion
-        minor_version        (u16  ) = { 0 }, // minorVersion
-        revision             (q32  ), // fontRevision
-        checksum_adjustment  (u32  ), // checkSumAdjustment
-        magic_number         (u32  ) = { 0x5F0F3CF5 }, // MagicNumber
-        flags                (Flags), // flags
-        units_per_em         (u16  ), // unitsPerEm
-        created              (i64  ), // created
-        modified             (i64  ), // modified
-        min_x                (i16  ), // xMin
-        min_y                (i16  ), // yMin
-        max_x                (i16  ), // xMax
-        max_y                (i16  ), // yMax
-        mac_style            (u16  ), // macStyle
-        lowest_ppem          (u16  ), // lowestRecPPEM
-        direction_hint       (i16  ), // fontDirectionHint
-        glyph_mapping_format (i16  ), // indexToLocFormat
-        glyph_data_format    (i16  ), // glyphDataFormat
+        major_version        (u16           ) = { 1 }, // majorVersion
+        minor_version        (u16           ) = { 0 }, // minorVersion
+        revision             (q32           ), // fontRevision
+        checksum_adjustment  (u32           ), // checkSumAdjustment
+        magic_number         (u32           ) = { 0x5F0F3CF5 }, // MagicNumber
+        flags                (Flags         ), // flags
+        units_per_em         (u16           ), // unitsPerEm
+        created              (i64           ), // created
+        modified             (i64           ), // modified
+        min_x                (i16           ), // xMin
+        min_y                (i16           ), // yMin
+        max_x                (i16           ), // xMax
+        max_y                (i16           ), // yMax
+        mac_style            (MacintoshFlags), // macStyle
+        lowest_ppem          (u16           ), // lowestRecPPEM
+        direction_hint       (i16           ), // fontDirectionHint
+        glyph_mapping_format (i16           ), // indexToLocFormat
+        glyph_data_format    (i16           ), // glyphDataFormat
     }
 }
 
@@ -35,5 +35,19 @@ flags! {
         0b0000_0000_0000_0001 => is_baseline_at_0,
         0b0000_0000_0000_0010 => is_left_side_bearing_at_0,
         0b1000_0000_0000_0000 => is_invalid,
+    }
+}
+
+flags! {
+    #[doc = "Macintosh style flags."]
+    pub MacintoshFlags(u16) {
+        0b0000_0000_0000_0001 => is_bold,
+        0b0000_0000_0000_0010 => is_italic,
+        0b0000_0000_0000_0100 => is_underline,
+        0b0000_0000_0000_1000 => is_outline,
+        0b0000_0000_0001_0000 => is_shadow,
+        0b0000_0000_0010_0000 => is_condensed,
+        0b0000_0000_0100_0000 => is_extended,
+        0b1111_1111_1000_0000 => is_invalid,
     }
 }
