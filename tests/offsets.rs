3a6c6fe53fba3acc6279f8ab5ecb44b5219ee923
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-01-31 14:57:08 +0100

    Add a write test for Offsets

--- tests/offsets.rs
+++ tests/offsets.rs
@@ -1,13 +1,35 @@
 #[macro_use]
 mod support;
 
+use std::io::{Cursor, Seek, SeekFrom};
+
 use truetype::tables::Offsets;
-use truetype::value::Read;
+use truetype::value::{Read, Write};
 
 #[test]
 fn read() {
     let mut file = setup!(SourceSerif);
-    let Offsets { header, records } = ok!(Offsets::read(&mut file));
+    let offsets = ok!(Offsets::read(&mut file));
+    test(&offsets, Some(&mut file));
+}
+
+#[test]
+fn write() {
+    let mut file = setup!(SourceSerif);
+    let offsets = ok!(Offsets::read(&mut file));
+
+    let mut cursor = Cursor::new(Vec::new());
+    ok!(offsets.write(&mut cursor));
+    ok!(cursor.seek(SeekFrom::Start(0)));
+
+    let offsets = ok!(Offsets::read(&mut cursor));
+    test::<Cursor<Vec<_>>>(&offsets, None);
+}
+
+fn test<T>(Offsets { header, records }: &Offsets, tape: Option<&mut T>)
+where
+    T: truetype::tape::Read,
+{
     assert_eq!(header.table_count, 12);
     assert_eq!(header.search_range, 8 * 16);
     assert_eq!(header.entry_selector, 3);
@@ -16,9 +38,11 @@ fn read() {
         header.table_count * 16 - header.search_range
     );
     assert_eq!(records.len(), 12);
-    let (_, failures): (Vec<_>, Vec<_>) = records
-        .iter()
-        .map(|record| (record, ok!(record.checksum(&mut file))))
-        .partition(|(record, checksum)| record.checksum == *checksum);
-    assert_eq!(failures.len(), 0, "{:?}", failures);
+    if let Some(tape) = tape {
+        let (_, failures): (Vec<_>, Vec<_>) = records
+            .iter()
+            .map(|record| (record, ok!(record.checksum(tape))))
+            .partition(|(record, checksum)| record.checksum == *checksum);
+        assert_eq!(failures.len(), 0);
+    }
 }
