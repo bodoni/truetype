72d2d1e87d6f0f834a10c5d75ae7a8be621d2664
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-02-06 10:09:54 +0100

    Add benchmarks for Names

---
+++ benches/names.rs
@@ -0,0 +1,41 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::fs::File;
+use std::io::Cursor;
+use std::path::PathBuf;
+
+use test::{black_box, Bencher};
+use truetype::tables::Names;
+use truetype::tape::Read as TapeRead;
+use truetype::tape::Write;
+use truetype::value::Read as ValueRead;
+
+macro_rules! ok(($result:expr) => ($result.unwrap()));
+
+#[bench]
+fn read(bencher: &mut Bencher) {
+    let path = PathBuf::from("tests")
+        .join("fixtures")
+        .join("OpenSans-Italic.ttf");
+    let mut file = ok!(File::open(path));
+    bencher.iter(|| {
+        ok!(file.jump(195040));
+        black_box(ok!(Names::read(&mut file)));
+    });
+}
+
+#[bench]
+fn write(bencher: &mut Bencher) {
+    let path = PathBuf::from("tests")
+        .join("fixtures")
+        .join("OpenSans-Italic.ttf");
+    let mut file = ok!(File::open(path));
+    ok!(file.jump(195040));
+    let table = ok!(Names::read(&mut file));
+    bencher.iter(|| {
+        let mut cursor = Cursor::new(vec![]);
+        black_box(ok!(cursor.give(&table)));
+    });
+}
